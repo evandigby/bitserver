f34c3761b76f3ccf7301c3d2691d345ec967d017
Evan Digby <evandigby@gmail.com>
2019-03-19 16:02:44 -0700

    updated to aggregate results

--- index.js
+++ index.js
@@ -5,7 +5,11 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 var serialport_1 = __importDefault(require("serialport"));
 var fs_1 = __importDefault(require("fs"));
-var serialport = new serialport_1.default("COM4", {
+var results = {
+    tab: {},
+    votesReceived: {},
+};
+var serialport = new serialport_1.default("COM6", {
     baudRate: 115200
 }, function (err) {
     if (err) {
@@ -13,6 +17,38 @@ var serialport = new serialport_1.default("COM4", {
         process.exit(1);
     }
 });
-serialport.on('data', function (data) {
-    fs_1.default.appendFileSync("file.txt", data + "\n");
-});
+function readLines(input, func) {
+    var remaining = '';
+    input.on('data', function (data) {
+        remaining += data;
+        var index = remaining.indexOf('\n');
+        while (index > -1) {
+            var line = remaining.substring(0, index);
+            remaining = remaining.substring(index + 1);
+            func(line);
+            index = remaining.indexOf('\n');
+        }
+    });
+    input.on('end', function () {
+        if (remaining.length > 0) {
+            func(remaining);
+        }
+    });
+}
+function func(data) {
+    var record = JSON.parse(data);
+    var vote = results.votesReceived[record.s];
+    if (vote) {
+        console.log('duplicate retrieve');
+        return;
+    }
+    results.votesReceived[record.s] = true;
+    var current = results.tab[record.v];
+    if (!current) {
+        current = 0;
+    }
+    current++;
+    results.tab[record.v] = current;
+    fs_1.default.writeFileSync("results.txt", JSON.stringify(results, null, "  "));
+}
+readLines(serialport, func);
--- index.ts
+++ index.ts
@@ -1,7 +1,19 @@
 import SerialPort from 'serialport';
 import fs from 'fs';
 
-const serialport = new SerialPort("COM4", {
+let results: {
+    tab: {
+        [key: string]: number | undefined
+    }
+    votesReceived: {
+        [key: number]: boolean
+    }
+} = {
+    tab: {},
+    votesReceived: {},
+};
+
+const serialport = new SerialPort("COM6", {
     baudRate: 115200
 },
     (err) => {
@@ -12,6 +24,46 @@ const serialport = new SerialPort("COM4", {
     }
 );
 
-serialport.on('data', (data) => {
-    fs.appendFileSync("file.txt", `${data}\n`);
-});
+function readLines(input: SerialPort, func: (data: string) => void) {
+    var remaining = '';
+
+    input.on('data', function (data) {
+        remaining += data;
+        var index = remaining.indexOf('\n');
+        while (index > -1) {
+            var line = remaining.substring(0, index);
+            remaining = remaining.substring(index + 1);
+            func(line);
+            index = remaining.indexOf('\n');
+        }
+    });
+
+    input.on('end', function () {
+        if (remaining.length > 0) {
+            func(remaining);
+        }
+    });
+}
+
+function func(data: string) {
+    let record: { t: number, s: number, v: any, n?: string } = JSON.parse(data);
+
+    let vote = results.votesReceived[record.s];
+    if (vote) {
+        return
+    }
+
+    results.votesReceived[record.s] = true;
+
+    let current = results.tab[record.v];
+    if (!current) {
+        current = 0;
+    }
+    current++;
+
+    results.tab[record.v] = current;
+
+    fs.writeFileSync("results.txt", JSON.stringify(results, null, "  "));
+}
+
+readLines(serialport, func);
